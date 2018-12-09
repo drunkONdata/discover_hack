d7ba776af36a5bc970c55a62f2b9abae501c678f
Diep Truong <29584874+dtruong11@users.noreply.github.com>
2018-12-08 22:39:29 -0800

    added react router

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+import HomePage from './components/HomePage'
+import Offers from './components/Offers'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/offers' component={Offers} />
+          <Redirect to="/" />
+        </Switch>
       </div>
     );
   }
