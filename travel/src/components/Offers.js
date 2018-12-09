89802afe111c3a06e56ba46b7a2f0a0c7551f31a
Diep Truong <29584874+dtruong11@users.noreply.github.com>
2018-12-09 00:50:47 -0800

    working on cards

---
+++ travel/src/components/Offers.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// The second screen in the MVP, list of offers 
+const Offers = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Offers 
